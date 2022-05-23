f9682936466f6c686b2c3ff3db869d9c54a04ead
Linh Van Thi My <vanmylink@gmail.com>
2022-05-23 10:59:35 +0700

    Created books table

---
+++ database/migrations/2022_05_23_030417_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('author');
+            $table->unsignedBigInteger('owner');
+            $table->tinyInteger('status')->default(0)->comment('0: available, 1: borrowing, 2: not available');
+            $table->timestamps();
+
+            $table->foreign('owner')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
